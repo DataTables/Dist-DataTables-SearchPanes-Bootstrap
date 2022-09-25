2d4d3e2dfef531b260b42b1d9d1a40506e4956be
Allan Jardine <allan@datatables.net>
2022-09-25 15:49:43 +0000

    Sync DistRepos

--- types/searchPanes.bootstrap.d.ts
+++ types/searchPanes.bootstrap.d.ts
@@ -1,7 +1,4 @@
 /*! Bootstrap integration for DataTables' SearchPanes
- * ©2016 SpryMedia Ltd - datatables.net/license
+ * © SpryMedia Ltd - datatables.net/license
  */
-declare let define: {
-    amd: string;
-    (stringValue: any, Function: any): any;
-};
+declare var DataTable: any;
