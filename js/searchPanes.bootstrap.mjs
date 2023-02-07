b37edcd6aea9b4f5d05a420bfff83bf9c09160a7
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-02-07 09:46:25 +0000

    7a502e46f166ad952847d32dcf91303a230a142b Fix: Clear all button wasn't working correctly in Bootstrap 5 and other styling integrations (search text wasn't being cleared)
    https://datatables.net/forums/discussion/75109
    
    Sync to source repo @7a502e46f166ad952847d32dcf91303a230a142b

--- js/searchPanes.bootstrap.mjs
+++ js/searchPanes.bootstrap.mjs
@@ -28,6 +28,7 @@ $.extend(true, DataTable.SearchPanes.classes, {
     clearAll: 'dtsp-clearAll btn btn-light',
     collapseAll: 'dtsp-collapseAll btn btn-light',
     disabledButton: 'disabled',
+    search: DataTable.SearchPane.classes.search,
     showAll: 'dtsp-showAll btn btn-light'
 });
 
